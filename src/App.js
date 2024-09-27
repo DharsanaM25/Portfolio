79bd1498a4f72502f7f62f93829af4948a1b372b
DHARSANA M <dharsana252@gmail.com>
2024-09-27 09:52:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Rendering from "./Rendering_work/Rendering";
+function App(){
+  return(
+    <>
+    <Rendering/>
+    </>
+  )
+}
+export default App;
---
+++ src/Rendering_work/Rendering.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Rendering(){
+    return(
+        <>
+        <h1>Hello...!</h1>
+        </>
+    )
+
+}
+export default Rendering;
